63432aedb8b559b286f1ae00912e756d4846c8b8
oem <oem@PC>
2021-01-24 18:31:17 +0200

    Add project files.

---
+++ Constructor/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer(); 
+        }
+    }
+    class Customer
+    {
+        //default Constructor
+        public Customer()
+        {
+            Console.WriteLine("Yapıcı blok çalıştı");
+
+        }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string City { get; set; }
+    }
+}
